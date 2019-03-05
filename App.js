50321c05501c36d7e6c96fa32f775e9bb43b8439
fujianlian <lianbinqiang@126.com>
2019-03-05 17:53:41 +0800

    Android启动页添加

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import SplashScreen from 'react-native-splash-screen'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -19,9 +20,15 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <Text style={styles.item}>SplashScreen 启动屏</Text>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
